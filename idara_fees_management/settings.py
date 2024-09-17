9fd33b64aa8ea40283f8e7fab9a893d3709a0847
iauth2024 <azadpashashaik@gmail.com>
2024-09-17 13:24:41 +0530

    settings

--- idara_fees_management/settings.py
+++ idara_fees_management/settings.py
@@ -154,7 +154,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'fees_collection',
 ]
 
 MIDDLEWARE = [
