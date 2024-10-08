a925a7fc977d43884f854900ca8b8311005aedcd
iauth2024 <azadpashashaik@gmail.com>
2024-10-08 17:27:05 +0530

    host

--- idara_fees_management/settings.py
+++ idara_fees_management/settings.py
@@ -135,7 +135,7 @@ SECRET_KEY = 'your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['idara-fees-management-1.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
